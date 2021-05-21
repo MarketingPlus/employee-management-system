5cfe7210951e5c2087b79645bceb87a69ebaebe3
MarketingPlus <benmarketingplus@gmail.com>
2021-05-21 20:06:41 +1000

    added some code so when users choice an option from the prompts it will connect to the database and return values from the db

--- database.js
+++ database.js
@@ -15,3 +15,34 @@ connection.connect(err => {
 	}
 	console.log(`connected as id ${connection.threadId}`);
 });
+
+const query = util.promisify(connection.query.bind(connection));
+
+class DB {
+	// READ
+	viewEmployees(type, answer) {
+		let searchQuery =
+			"SELECT e.id, e.first_name AS 'First Name', e.last_name AS 'Last Name', title AS Title, ";
+		searchQuery +=
+			"salary AS Salary, name AS Department, CONCAT(m.first_name, ' ', m.last_name) AS Manager ";
+		searchQuery += "FROM employee e ";
+		searchQuery += "INNER JOIN role ON e.role_id = role.id ";
+		searchQuery +=
+			"INNER JOIN department ON role.department_id = department.id ";
+		searchQuery += "LEFT JOIN employee m ON e.manager_id = m.id ";
+
+		if (type === "department") {
+			searchQuery += "WHERE department.name = ? ";
+		}
+
+		if (type === "manager") {
+			searchQuery += "WHERE CONCAT(m.first_name, ' ', m.last_name) = ? ";
+		}
+
+		searchQuery += "ORDER BY e.id ASC";
+
+		return query(searchQuery, answer);
+	}
+}
+
+module.exports = new DB();
